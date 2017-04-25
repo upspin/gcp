89b11b35ae4b6458212584c8ac155438e43f4685
Andrew Gerrand <adg@golang.org>
2017-04-25 13:28:57 +1000

    cmd/frontend-gcp: add GCP wrapper for frontend binary
    The only distinction between this and cmd/frontend is that frontend-gcp
    can store its Let's Encrypt certificates in a Cloud Storage bucket.
    
    Change-Id: I05a7885fa6cd7a9997fd1dfa6b8ab53b73603d12

---
+++ cmd/frontend-gcp/main.go
@@ -0,0 +1,17 @@
+// Copyright 2017 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Frontend provides a web server that serves documentation and meta
+// tags to instruct "go get" where to find the upspin source repository.
+package main // import "gcp.upspin.io/cmd/frontend-gcp"
+
+import (
+	"gcp.upspin.io/cloud/https"
+	"upspin.io/serverutil/frontend"
+)
+
+func main() {
+	frontend.Main()
+	https.ListenAndServe(nil, "frontend")
+}
