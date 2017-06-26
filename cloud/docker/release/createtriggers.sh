9d94fdf4c290ecef7b975dfc885a7f65c87456c3
Andrew Gerrand <adg@golang.org>
2017-06-26 16:06:56 +1000

    cloud/docker/release: add mechanism to build Upspin binaries
    This set of scripts and Dockerfiles provides a Google Container Builder
    configuration for automatically building and distributing new versions
    of the Upspin binaries whenever a new commit is pushed to the upspin
    repository. See the README file for details.
    
    Change-Id: I84f802347ed3479f806f6cabadd213e2a1adc9bb

---
+++ cloud/docker/release/createtriggers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script creates the Google Container Builder trigger to build release
+# binaries on each push to the upspin repo's master branch.
+# It should be called by an administrator.
+
+project=upspin-prod
+auth="$(gcloud config config-helper --format='value(credential.access_token)')"
+sed "s/PROJECT_ID/$project/" trigger.yaml | curl -X POST -T - \
+	-H "Authorization: Bearer $auth" \
+	https://cloudbuild.googleapis.com/v1/projects/$project/triggers
