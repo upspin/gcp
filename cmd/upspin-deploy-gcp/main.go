upspin/gcp

commit d7fa3b6455c79279e517d202b754b1dd59b7f7b7

cmd/hostserver-gcp: new service to provision sub-domains
Command hostserver-gcp is a combined DirServer and StoreServer that serves a
synthetic Upspin tree used to configure sub-domains under the domain
upspin.services. Each Upspin user can set the IP address of one sub-domain,
the name of which is a hash of their user name.

Assuming the server is running as host@upspin.io, here's how the user
user@example.com would configure their sub-domain:

  $ upspin mkdir host@upspin.io/user@example.com/127.0.0.1

If that command succeeds, then their host name was created or updated.
(This can be any command that performs a DirServer.Put to that path; the put
command would work, but mkdir is the simpliest choice in this case.)

To find the host name, the user uses a get request, which returns both the
host name and configured IP address:

  $ upspin get host@example.com/user@example.com
  127.0.0.1
  b4c9a289323b21a01c3e940f150eb9b8.upspin.services

This is part of addressing upspin/upspin#411. If a user wants to deploy
their own upspinserver then they must give it a host name in order to
serve TLS. By providing each Upspin user with their own dedicated host
name under upspin.services, we side-step the issue of domain
registration and DNS record configuration (although we can let them use
their own domain if they wish).

The service is already running at host@upspin.io.

Change-Id: Ib861155e7e24449522db7f28126a3ba6f765a66d
Reviewed-on: https://upspin-review.googlesource.com/12120
Reviewed-by: Rob Pike <r@golang.org>

cmd/upspin-deploy-gcp/main.go
@@ -205,7 +205,7 @@ type Config struct {
 }
 
 var (
-	validServers   = []string{"dirserver", "storeserver", "keyserver", "frontend"}
+	validServers   = []string{"dirserver", "storeserver", "keyserver", "frontend", "hostserver"}
 	defaultServers = []string{"dirserver", "storeserver"}
 )
 
@@ -460,34 +460,23 @@ func (c *Config) buildServer(server string) error {
 			"secrets: /upspin",
 			"keyserver: "+c.endpoint("keyserver"),
 		)
+	case "hostserver":
+		err = writeConfig(dir,
+			"username: "+c.hostServerUserName(),
+			"secrets: /upspin",
+			"keyserver: "+c.endpoint("keyserver"),
+		)
 	}
 	if err != nil {
 		return err
 	}
 
-	var files []string
-	switch server {
-	case "dirserver":
-		files = []string{
-			"public.upspinkey",
-			"secret.upspinkey",
-		}
-	case "keyserver":
-		files = []string{
-			"mailconfig",
-			"public.upspinkey",
-			"secret.upspinkey",
-		}
-	case "storeserver":
-		files = []string{
-			"public.upspinkey",
-			"secret.upspinkey",
-		}
-	case "frontend":
-		files = []string{
-			"public.upspinkey",
-			"secret.upspinkey",
-		}
+	files := []string{
+		"public.upspinkey",
+		"secret.upspinkey",
+	}
+	if server == "hostserver" {
+		files = append(files, "serviceaccount.json")
 	}
 	home, err := config.Homedir()
 	if err != nil {
@@ -588,6 +577,10 @@ func (c *Config) frontendUserName() string {
 		return "upspin-frontend@upspin.io"
 	}
 	return "upspin-frontend@" + c.Domain
+}
+
+func (c *Config) hostServerUserName() string {
+	return "host@upspin.io"
 }
 
 func writeConfig(dir string, lines ...string) error {
@@ -758,6 +751,8 @@ func (c *Config) hostName(server string) string {
 		host = "dir"
 	case "frontend":
 		return c.Domain
+	case "hostserver":
+		host = "host"
 	default:
 		panic("unknown server: " + server)
 	}

cmd/hostserver-gcp/main.go
@@ -1,0 +1,60 @@
+// Copyright 2017 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Command hostserver-gcp is a combined DirServer and StoreServer that serves a
+// synthetic Upspin tree used to configure sub-domains under the domain
+// upspin.services. Each Upspin user can set the IP address of one sub-domain,
+// the name of which is a hash of their user name.
+//
+// Assuming the server is running as host@upspin.io, here's how the user
+// user@example.com would configure their sub-domain:
+//   $ upspin mkdir host@upspin.io/user@example.com/127.0.0.1
+// If that command succeeds, their host name was created or updated.
+// (This can be any command that performs a DirServer.Put to that path; the put
+// command would work, but mkdir is the simpliest choice in this case.)
+//
+// To find the host name, the user issues a get request, which returns both the
+// host name and configured IP address:
+//   $ upspin get host@example.com/user@example.com
+//   127.0.0.1
+//   b4c9a289323b21a01c3e940f150eb9b8.upspin.services
+//
+package main // import "gcp.upspin.io/cmd/hostserver-gcp"
+
+import (
+	"log"
+	"net/http"
+
+	"gcp.upspin.io/cloud/https"
+
+	"upspin.io/config"
+	"upspin.io/flags"
+	"upspin.io/rpc/dirserver"
+	"upspin.io/rpc/storeserver"
+	"upspin.io/upspin"
+)
+
+func main() {
+	flags.Parse(flags.Server)
+
+	addr := upspin.NetAddr(flags.NetAddr)
+	ep := upspin.Endpoint{
+		Transport: upspin.Remote,
+		NetAddr:   addr,
+	}
+	cfg, err := config.FromFile(flags.Config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s, err := newServer(ep, cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.Handle("/api/Store/", storeserver.New(cfg, s.StoreServer(), addr))
+	http.Handle("/api/Dir/", dirserver.New(cfg, s.DirServer(), addr))
+
+	https.ListenAndServe(nil, "hostserver")
+}

cloud/docker/hostserver/Dockerfile
@@ -1,0 +1,8 @@
+FROM buildpack-deps:jessie-curl
+COPY config /upspin/config
+COPY public.upspinkey /upspin/public.upspinkey
+COPY secret.upspinkey /upspin/secret.upspinkey
+COPY serviceaccount.json /upspin/serviceaccount.json
+COPY bin/hostserver-gcp /hostserver-gcp
+ENTRYPOINT ["/hostserver-gcp", "-letscache=", "-https=:443", "-config=/upspin/config", "-addr=HOSTNAME:443"]
+EXPOSE 443

cloud/kube/deployment/hostserver.yaml
@@ -1,0 +1,16 @@
+kind: Deployment
+apiVersion: extensions/v1beta1
+metadata:
+  name: PREFIXhostserver
+spec:
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        app: PREFIXhostserver
+    spec:
+      containers:
+      - name: PREFIXhostserver
+        image: gcr.io/PROJECT/PREFIXhostserver
+        ports:
+        - containerPort: 443

cloud/kube/service/hostserver.yaml
@@ -1,0 +1,14 @@
+kind: Service
+apiVersion: v1
+metadata:
+  name: "PREFIXhostserver"
+spec:
+  selector:
+    app: "PREFIXhostserver"
+  ports:
+  - name: "https"
+    port: 443
+    targetPort: 443
+    protocol: "TCP"
+  type: LoadBalancer
+  loadBalancerIP: IPADDR

cmd/hostserver-gcp/dns.go
@@ -1,0 +1,121 @@
+// Copyright 2017 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"context"
+	"crypto/sha256"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"golang.org/x/oauth2/google"
+	dns "google.golang.org/api/dns/v1"
+	"google.golang.org/api/googleapi"
+
+	"upspin.io/errors"
+	"upspin.io/upspin"
+)
+
+// TODO(adg): make this configurable?
+const (
+	dnsProject = "upspin-prod"
+	dnsZone    = "upspin-services"
+	dnsDomain  = "upspin.services"
+)
+
+// userToHost converts an Upspin user name to a fully-qualified domain name
+// under the upspin.services domain. The host portion of the name is the
+// hex-encoded first 16 bytes of the SHA256 checksum of the user name.
+// The security of this service relies on there not being collisions in this
+// space, which should be astronomically unlikely.
+func userToHost(name upspin.UserName) string {
+	hash := sha256.New()
+	hash.Write([]byte(name))
+	return fmt.Sprintf("%x."+dnsDomain, hash.Sum(nil)[:16])
+}
+
+// setupDNSService loads the credentials for accessing the Cloud DNS service
+// and sets the server's dnsSvc with a ready-to-use dns.Service.
+func (s *server) setupDNSService() error {
+	ctx := context.Background()
+	var client *http.Client
+
+	// First try to read the serviceaccount.json in the Docker image.
+	b, err := ioutil.ReadFile("/upspin/serviceaccount.json")
+	if err == nil {
+		cfg, err := google.JWTConfigFromJSON(b, dns.CloudPlatformScope)
+		if err != nil {
+			return err
+		}
+		client = cfg.Client(ctx)
+	} else if os.IsNotExist(err) {
+		// Otherwise use the default application credentials,
+		// which should work when testing locally.
+		client, err = google.DefaultClient(ctx, dns.CloudPlatformScope)
+		if err != nil {
+			return err
+		}
+	} else {
+		return err
+	}
+
+	s.dnsSvc, err = dns.New(client)
+	return err
+}
+
+// listRecordSets returns the list of record sets for a given host name.
+func (s *server) listRecordSets(host string) ([]*dns.ResourceRecordSet, error) {
+	resp, err := s.dnsSvc.ResourceRecordSets.List(dnsProject, dnsZone).Name(host + ".").Do()
+	if err != nil {
+		return nil, err
+	}
+	return resp.Rrsets, nil
+}
+
+// lookupName returns the IP address and host name for a given user, or a
+// NotExist error if there is no host name for that user.
+func (s *server) lookupName(name upspin.UserName) (ip, host string, err error) {
+	host = userToHost(name)
+
+	rrsets, err := s.listRecordSets(host)
+	if err != nil {
+		return "", "", err
+	}
+	for _, rrs := range rrsets {
+		for _, rrd := range rrs.Rrdatas {
+			return rrd, host, nil
+		}
+	}
+
+	return "", "", errors.E(errors.NotExist)
+}
+
+// updateName creates (or replaces) an A record for the given user's host name
+// that points to the given IP address, and returns the user's host name.
+func (s *server) updateName(name upspin.UserName, ip string) (host string, err error) {
+	host = userToHost(name)
+
+	rrsets, err := s.listRecordSets(host)
+	if err != nil {
+		return "", err
+	}
+	change := &dns.Change{
+		Additions: []*dns.ResourceRecordSet{{
+			Name:    host + ".",
+			Rrdatas: []string{ip},
+			Ttl:     3600, // 1 hour
+			Type:    "A",
+		}},
+		Deletions: rrsets,
+	}
+	change, err = s.dnsSvc.Changes.Create(dnsProject, dnsZone, change).Do()
+	if err != nil && !googleapi.IsNotModified(err) {
+		return "", err
+	}
+
+	return host, nil
+}

cmd/hostserver-gcp/hostserver.upbox
@@ -1,0 +1,15 @@
+users:
+  - name: user
+  - name: host
+    dirserver: $hostserver-gcp
+    storeserver: $hostserver-gcp
+
+servers:
+  - name: keyserver
+  - name: storeserver
+  - name: dirserver
+  - name: hostserver-gcp
+    user: host
+    importpath: gcp.upspin.io/cmd/hostserver-gcp
+
+domain: example.com

cmd/hostserver-gcp/server.go
@@ -1,0 +1,305 @@
+// Copyright 2017 The Upspin Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	dns "google.golang.org/api/dns/v1"
+
+	"upspin.io/access"
+	"upspin.io/cache"
+	"upspin.io/errors"
+	"upspin.io/pack"
+	"upspin.io/path"
+	"upspin.io/upspin"
+	"upspin.io/valid"
+
+	_ "upspin.io/pack/ee"
+	_ "upspin.io/transports"
+)
+
+// state holds the state for a combined directory and store server that serves
+// a file system for interacting with the Cloud DNS service that does DNS
+// resolution for the upspin.services domain.
+// See the package comment for a full description of the file system.
+// A single state struct is shared by all instances of the server (see below).
+type state struct {
+	ep  upspin.Endpoint
+	cfg upspin.Config
+
+	accessEntry *upspin.DirEntry
+	accessBytes []byte
+
+	// cache is a cache of recently-packed entries keyed by file path.
+	// It also acts as a "negative" cache for non-existent entries, in
+	// which case the cached value is nil.
+	cache *cache.LRU // [filePath]*entry
+
+	dnsSvc *dns.Service
+}
+
+// server provides a wrapper around state that provies the upspin.DirServer and
+// upspin.StoreServer methods for a particular user.
+// A new wrapper is created by each Dial.
+type server struct {
+	user upspin.UserName // Set by Dial.
+
+	// state is the state struct shared by all instances of the server.
+	*state
+}
+
+// entry represents a cached DirEntry and its underlying data.
+type entry struct {
+	de   *upspin.DirEntry
+	data []byte
+}
+
+type dirServer struct {
+	*server
+}
+
+type storeServer struct {
+	*server
+}
+
+func (s *server) DirServer() upspin.DirServer {
+	return dirServer{s}
+}
+
+func (s *server) StoreServer() upspin.StoreServer {
+	return storeServer{s}
+}
+
+const (
+	accessRef        = upspin.Reference(access.AccessFile)
+	packing          = upspin.EEPack
+	maxCachedEntries = 1000
+)
+
+var accessRefdata = upspin.Refdata{Reference: accessRef}
+
+func newServer(ep upspin.Endpoint, cfg upspin.Config) (*server, error) {
+	s := &server{
+		state: &state{
+			ep:    ep,
+			cfg:   cfg,
+			cache: cache.NewLRU(maxCachedEntries),
+		},
+	}
+
+	err := s.setupDNSService()
+	if err != nil {
+		return nil, err
+	}
+
+	// Allow anyone to write, but only the server user to read.
+	const accessFile = "read, write: all\n"
+	s.accessEntry, s.accessBytes, err = s.pack(access.AccessFile, []byte(accessFile))
+	if err != nil {
+		return nil, err
+	}
+
+	return s, nil
+}
+
+// pack uses packer to pack the named file with the given data and returns the
+// resulting DirEntry and ciphertext.
+func (s *server) pack(filePath string, data []byte) (*upspin.DirEntry, []byte, error) {
+	name := path.Join(upspin.PathName(s.cfg.UserName()), filePath)
+	de := &upspin.DirEntry{
+		Writer:     s.cfg.UserName(),
+		Name:       name,
+		SignedName: name,
+		Packing:    packing,
+		Time:       upspin.Now(),
+		Sequence:   1,
+	}
+
+	packer := pack.Lookup(packing)
+
+	bp, err := packer.Pack(s.cfg, de)
+	if err != nil {
+		return nil, nil, err
+	}
+	cipher, err := bp.Pack(data)
+	if err != nil {
+		return nil, nil, err
+	}
+	bp.SetLocation(upspin.Location{
+		Endpoint:  s.ep,
+		Reference: upspin.Reference(filePath),
+	})
+	if err := bp.Close(); err != nil {
+		return nil, nil, err
+	}
+
+	// Share with all.
+	packer.Share(s.cfg, []upspin.PublicKey{upspin.AllUsersKey}, []*[]byte{&de.Packdata})
+
+	return de, cipher, nil
+}
+
+// packHost packs a file for the user containing the host and IP address.
+// It then returns the entry after adding it to the cache.
+func (s *server) packHost(name upspin.UserName, ip, host string) (e *entry, err error) {
+	e = &entry{}
+	e.de, e.data, err = s.pack(string(name), []byte(fmt.Sprintf("%s\n%s\n", ip, host)))
+	if err != nil {
+		return nil, err
+	}
+	s.cache.Add(name, e)
+	return e, nil
+}
+
+// lookup returns the entry for the given user. It first consults the cache.
+// If the entry is unknown, it invokes lookupName to find the user's host
+// information, packs the resulting data, and updates the cache.
+func (s *server) lookup(name upspin.UserName) (*entry, error) {
+	if err := valid.UserName(name); err != nil {
+		return nil, err
+	}
+	ei, ok := s.cache.Get(name)
+	if ok {
+		if ei == nil {
+			return nil, errors.E(name, errors.NotExist)
+		}
+		return ei.(*entry), nil
+	}
+
+	ip, host, err := s.lookupName(name)
+	if errors.Match(errors.E(errors.NotExist), err) {
+		s.cache.Add(name, nil)
+		return nil, err
+	}
+	if err != nil {
+		return nil, err
+	}
+	return s.packHost(name, ip, host)
+}
+
+// These methods implement upspin.Service.
+
+func (s *server) Endpoint() upspin.Endpoint { return s.ep }
+func (*server) Close()                      {}
+
+// These methods implement upspin.Dialer.
+
+func (s storeServer) Dial(cfg upspin.Config, _ upspin.Endpoint) (upspin.Service, error) {
+	return (&server{
+		user:  cfg.UserName(),
+		state: s.state,
+	}).StoreServer(), nil
+}
+
+func (s dirServer) Dial(cfg upspin.Config, _ upspin.Endpoint) (upspin.Service, error) {
+	return (&server{
+		user:  cfg.UserName(),
+		state: s.state,
+	}).DirServer(), nil
+}
+
+// These methods implement upspin.DirServer.
+
+func (s dirServer) Lookup(name upspin.PathName) (*upspin.DirEntry, error) {
+	if name == s.accessEntry.Name {
+		return s.accessEntry, nil
+	}
+
+	p, err := path.Parse(name)
+	if err != nil {
+		return nil, err
+	}
+	if p.User() != s.cfg.UserName() {
+		return nil, errors.E(name, errors.NotExist)
+	}
+	if p.FilePath() == "" {
+		return &upspin.DirEntry{
+			SignedName: name,
+			Attr:       upspin.AttrDirectory,
+			Packing:    upspin.PlainPack,
+			Time:       upspin.Now(),
+			Name:       name,
+			Writer:     s.cfg.UserName(),
+		}, nil
+	}
+	e, err := s.lookup(upspin.UserName(p.FilePath()))
+	if err != nil {
+		return nil, errors.E(name, err)
+	}
+	return e.de, nil
+}
+
+func (s dirServer) Glob(pattern string) ([]*upspin.DirEntry, error) {
+	// Nobody has list access.
+	return nil, errors.E(errors.Private)
+}
+
+func (s dirServer) Put(de *upspin.DirEntry) (*upspin.DirEntry, error) {
+	if err := valid.DirEntry(de); err != nil {
+		return nil, err
+	}
+
+	p, err := path.Parse(de.Name)
+	if err != nil {
+		return nil, err
+	}
+	if p.User() != s.cfg.UserName() {
+		return nil, errors.E(de.Name, errors.NotExist)
+	}
+	parts := strings.Split(p.FilePath(), "/")
+	if len(parts) != 2 {
+		return nil, errors.E(errors.Permission, de.Name, errors.Str("file names must be of the form user@example.com/ip"))
+	}
+	user, ip := upspin.UserName(parts[0]), parts[1]
+	if user != s.user {
+		return nil, errors.E(errors.Permission, de.Name)
+	}
+
+	host, err := s.updateName(user, ip)
+	if err != nil {
+		return nil, err
+	}
+	_, err = s.packHost(user, ip, host)
+	return nil, err
+}
+
+func (s dirServer) WhichAccess(name upspin.PathName) (*upspin.DirEntry, error) {
+	return s.accessEntry, nil
+}
+
+// This method implements upspin.StoreServer.
+
+func (s storeServer) Get(ref upspin.Reference) ([]byte, *upspin.Refdata, []upspin.Location, error) {
+	if ref == accessRef {
+		return s.accessBytes, &accessRefdata, nil, nil
+	}
+	e, err := s.lookup(upspin.UserName(ref))
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	return e.data, &upspin.Refdata{Reference: ref, Volatile: true}, nil, nil
+}
+
+// The DirServer and StoreServer methods below are not implemented.
+
+var errNotImplemented = errors.E(errors.Invalid, errors.Str("method not implemented"))
+
+func (dirServer) Delete(name upspin.PathName) (*upspin.DirEntry, error) {
+	return nil, errNotImplemented
+}
+
+func (dirServer) Watch(_ upspin.PathName, _ int64, _ <-chan struct{}) (<-chan upspin.Event, error) {
+	return nil, upspin.ErrNotSupported
+}
+
+func (storeServer) Put(data []byte) (*upspin.Refdata, error) {
+	return nil, errNotImplemented
+}
+
+func (storeServer) Delete(ref upspin.Reference) error {
+	return errNotImplemented
+}

commit 7d3ecd8f07bc876c8c855b8c6964c9e827abd51e

cmd/upspin-deploy-gcp: include mailconfig in keyserver deploy
Change d7fa3b6 accidentally removed the inclusion of the mailconfig file
when preparing the keyserver Docker image. Restore that behavior.

Change-Id: I93387b7d944e0c60a8c0365e1831dcc616d3fa58

cmd/upspin-deploy-gcp/main.go
@@ -483,6 +483,9 @@ func (c *Config) buildServer(server string) error {
 		"public.upspinkey",
 		"secret.upspinkey",
 	}
+	if server == "keyserver" {
+		files = append(files, "mailconfig")
+	}
 	if server == "hostserver" {
 		files = append(files, "serviceaccount.json")
 	}