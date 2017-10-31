bd7e28062aaeaa54e1aca3c114048d19ca3b1a3d
Andrew Gerrand <adg@golang.org>
2017-10-31 15:29:48 +1100

    cloud/docker/upspinserver-build: automatically build upspinserver
    This change adds the scripts to create a build trigger to automatically
    build a new gcr.io/upspin-containers/upspinserver Docker image when a
    new commit is made to the gcp repo.
    
    This Docker image is used by VMs created by the upspin-ui tool.
    
    Change-Id: Ie116b2e08cc2464539d087af86d68e36803b9b9f

---
+++ cloud/docker/upspinserver-build/mkupspinserver.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+# This script runs inside the upspinserver-build Docker container.
+# It copies the gcp.uspin.io tree out of /workspace (a volume provided by
+# Google Container Builder) and into GOPATH, builds upspinserver-gcp, and puts
+# the resulting binary in /workspace/bin.
+
+cp -R /workspace /go/src/gcp.upspin.io
+rm -r /workspace/*
+cp /go/src/gcp.upspin.io/cloud/docker/upspinserver/* /workspace
+mkdir /workspace/bin
+/usr/local/go/bin/go generate -run make_version gcp.upspin.io/vendor/upspin.io/version
+/usr/local/go/bin/go build -o /workspace/bin/upspinserver-gcp gcp.upspin.io/cmd/upspinserver-gcp
