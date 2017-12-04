79510441dd957ebcb6b72204bff226276f1b5c60
Andrew Gerrand <adg@golang.org>
2017-12-04 16:35:03 +1100

    cloud/storage/gcs: implement storage.Lister
    Updates upspin/upspin#145
    
    Change-Id: Ibc792451b56f04131f2fed2e5e43d0f6be9820e0
    Reviewed-on: https://upspin-review.googlesource.com/16882
    Reviewed-by: Rob Pike <r@golang.org>

--- cloud/storage/gcs/gcs_test.go
+++ cloud/storage/gcs/gcs_test.go
@@ -13,6 +13,7 @@ import (
 
 	"upspin.io/cloud/storage"
 	"upspin.io/log"
+	"upspin.io/upspin"
 )
 
 const defaultTestBucketName = "upspin-test-scratch"
@@ -65,6 +66,68 @@ func TestDelete(t *testing.T) {
 	_, err = client.Download(fileName)
 	if err == nil {
 		t.Fatal("Expected an error, but got none")
+	}
+}
+
+func TestList(t *testing.T) {
+	ls, ok := client.(storage.Lister)
+	if !ok {
+		t.Fatal("impl does not provide List method")
+	}
+
+	if err := client.(*gcsImpl).emptyBucket(false); err != nil {
+		t.Fatal(err)
+	}
+
+	refs, next, err := ls.List("")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(refs) != 0 {
+		t.Errorf("list of empty bucket returned %d refs", len(refs))
+	}
+	if next != "" {
+		t.Errorf("list of empty bucket returned non-empty page token %q", next)
+	}
+
+	// Test pagination by reducing the results per page to 2.
+	oldMaxResults := maxResults
+	defer func() { maxResults = oldMaxResults }()
+	maxResults = 2
+
+	const nFiles = 6 // Must be evenly divisible by maxResults.
+	for i := 0; i < nFiles; i++ {
+		err = client.Put(fmt.Sprintf("test-%d", i), testData)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	seen := make(map[upspin.Reference]bool)
+	for i := 0; i < nFiles/2; i++ {
+		refs, next, err = ls.List(next)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(refs) != 2 {
+			t.Errorf("got %d refs, want 2", len(refs))
+		}
+		if i == nFiles/2-1 {
+			if next != "" {
+				t.Errorf("got page token %q, want empty", next)
+			}
+		} else if next == "" {
+			t.Error("got empty page token, want non-empty")
+		}
+		for _, ref := range refs {
+			if seen[ref.Ref] {
+				t.Errorf("saw duplicate ref %q", ref.Ref)
+			}
+			seen[ref.Ref] = true
+			if got, want := ref.Size, int64(len(testData)); got != want {
+				t.Errorf("ref %q has size %d, want %d", ref.Ref, got, want)
+			}
+		}
 	}
 }
 
