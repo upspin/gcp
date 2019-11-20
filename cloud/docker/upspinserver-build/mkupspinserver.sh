f5a2162c9e2a1b0ed121fd6fe6311749fc80ef77
Andrew Gerrand <adg@golang.org>
2019-11-20 11:47:46 +1100

    cloud/docker: disable makeversion for upspinserver and upspin-ui
    This breaks the -version flag for the distributed upspinserver and
    upspin-ui binaries; the approach we were using here does not work quite
    the same with Go modules. I will fix this in a subsequent change, but
    for now this fixes our continuous deployment.
    
    Also add some helpful messages to makerelease.sh.
    
    Change-Id: Ia382db22473780bcbb083209eefabef3474d3483

--- cloud/docker/upspinserver-build/mkupspinserver.sh
+++ cloud/docker/upspinserver-build/mkupspinserver.sh
@@ -5,9 +5,23 @@
 # Google Container Builder) and into GOPATH, builds upspinserver-gcp, and puts
 # the resulting binary in /workspace/bin.
 
+echo "Copying /workspace to $GOPATH/src/gcp.upspin.io"
 cp -R /workspace /go/src/gcp.upspin.io
+echo "Emptying workspace"
 rm -r /workspace/*
-cp /go/src/gcp.upspin.io/cloud/docker/upspinserver/* /workspace
+
+echo GOPATH=$GOPATH
+find $GOPATH -type f
+
+echo "Copying upspinserver artifacts to workspace"
+cp $GOPATH/src/gcp.upspin.io/cloud/docker/upspinserver/* /workspace
 mkdir /workspace/bin
-/usr/local/go/bin/go generate -run make_version gcp.upspin.io/vendor/upspin.io/version
+
+cd $GOPATH/src/gcp.upspin.io
+
+# TODO(adg): restore this functionality
+#echo "Generating version package"
+#/usr/local/go/bin/go generate -run make_version gcp.upspin.io/vendor/upspin.io/version
+
+echo "Building upspinserver-gcp"
 /usr/local/go/bin/go build -o /workspace/bin/upspinserver-gcp gcp.upspin.io/cmd/upspinserver-gcp
